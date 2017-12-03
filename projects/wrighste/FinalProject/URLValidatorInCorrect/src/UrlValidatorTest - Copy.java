63884b12a99680ac653cc6f47c2fbb5d5560d395
Wright <swright@corp.intuit.net>
2017-12-02 23:04:34 -0600

    combine loop written by teammater and random string generator

--- projects/wrighste/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest - Copy.java
+++ projects/wrighste/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest - Copy.java
@@ -207,6 +207,259 @@ public class UrlValidatorTest extends TestCase {
 
 	}
 
+	public void RandomtestIsValid() {
+		int schemeArrayLoop = 0;
+		int allowAllSchemeLoop = 0;
+		int allow2SlashesFlag = 0;
+		// valid AND existing schemese, found here :
+		// https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml only last one
+		// is valid and non-existing
+
+		ArrayList DefaultSchemes = new ArrayList();
+		DefaultSchemes.add("http");
+		DefaultSchemes.add("https");
+		DefaultSchemes.add("ftp");
+
+		String[] validCharArray = { "" };
+		String[] InvalidCharArray;
+
+		ArrayList ValidSchemes = new ArrayList();
+		ValidSchemes.add("Go");
+		ValidSchemes.add("Gopher");
+		ValidSchemes.add("Http");
+		ValidSchemes.add("Https");
+		ValidSchemes.add("hxxp");
+		ValidSchemes.add("ms-secondary-screen-controller");
+		ValidSchemes.add("ni");
+		ValidSchemes.add("soap.beep");
+		ValidSchemes.add("telnet");
+		ValidSchemes.add("BooBooKitty");
+
+		ArrayList InValidSchemes = new ArrayList();
+		InValidSchemes.add("Go<");
+		InValidSchemes.add("Go pher");
+		InValidSchemes.add("Htt#");
+		InValidSchemes.add("H%tps");
+		InValidSchemes.add("hx#p");
+		InValidSchemes.add("ms<secondary-screen-controller");
+		InValidSchemes.add("=");
+		InValidSchemes.add("soap[.beep");
+		InValidSchemes.add("]elnet");
+		InValidSchemes.add("B'oBooKitty");
+
+		ArrayList ValidChars = new ArrayList();
+		ValidChars.add("A");
+		ValidChars.add("a");
+		ValidChars.add("B");
+		ValidChars.add("b");
+		ValidChars.add("C");
+		ValidChars.add("c");
+		ValidChars.add("D");
+		ValidChars.add("d");
+		ValidChars.add("E");
+		ValidChars.add("e");
+		ValidChars.add("F");
+		ValidChars.add("f");
+		ValidChars.add("G");
+		ValidChars.add("g");
+		ValidChars.add("H");
+		ValidChars.add("h");
+		ValidChars.add("I");
+		ValidChars.add("i");
+		ValidChars.add("J");
+		ValidChars.add("j");
+		ValidChars.add("K");
+		ValidChars.add("k");
+		ValidChars.add("L");
+		ValidChars.add("l");
+		ValidChars.add("M");
+		ValidChars.add("m");
+		ValidChars.add("N");
+		ValidChars.add("n");
+		ValidChars.add("O");
+		ValidChars.add("o");
+		ValidChars.add("P");
+		ValidChars.add("p");
+		ValidChars.add("Q");
+		ValidChars.add("q");
+		ValidChars.add("R");
+		ValidChars.add("r");
+		ValidChars.add("S");
+		ValidChars.add("s");
+		ValidChars.add("T");
+		ValidChars.add("t");
+		ValidChars.add("U");
+		ValidChars.add("u");
+		ValidChars.add("V");
+		ValidChars.add("v");
+		ValidChars.add("W");
+		ValidChars.add("w");
+		ValidChars.add("X");
+		ValidChars.add("x");
+		ValidChars.add("Y");
+		ValidChars.add("y");
+		ValidChars.add("Z");
+		ValidChars.add("z");
+		ValidChars.add("0");
+		ValidChars.add("1");
+		ValidChars.add("2");
+		ValidChars.add("3");
+		ValidChars.add("4");
+		ValidChars.add("5");
+		ValidChars.add("6");
+		ValidChars.add("7");
+		ValidChars.add("8");
+		ValidChars.add("9");
+		ValidChars.add("-");
+		ValidChars.add(".");
+		ValidChars.add("~");
+
+		ArrayList InValidChars = new ArrayList();
+		InValidChars.add("!");
+		InValidChars.add("*");
+		InValidChars.add(" ' ");
+		InValidChars.add("(");
+		InValidChars.add(")");
+		InValidChars.add(";");
+		InValidChars.add("/");
+		InValidChars.add("?");
+		InValidChars.add("#");
+		InValidChars.add("[");
+		InValidChars.add("]");
+		// UrlValidator urlVal = new UrlValidator();
+		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+		int validityFlag = 0; // 0 = valid chars in scheme and rest of url, 1 = invalid scheme, other chars
+								// are ok, 2 = scheme is valid, rest of URL is not, 3 = scheme and rest of URL
+								// is invalid
+		int urlLen = 0;
+
+		String baseString = "";
+		String errorMessage = "";
+		boolean expectedisValidResult = false;
+		boolean result;
+		for (int i = 0; i < 100; i++) {
+			validityFlag = i % 4;
+			urlLen = (i % 10) + 1; // limit to 10 len url for our testing, never less than 1 char
+			if (urlLen == 1) {
+				Collections.shuffle(ValidChars); // randomize every 10 loops
+				Collections.shuffle(InValidChars);
+				Collections.shuffle(ValidSchemes);
+				Collections.shuffle(InValidSchemes);
+			}
+
+			if (validityFlag == 0) { // 0 = valid chars in scheme and rest of url,
+				errorMessage = "Valid Scheme and Valid URL text ";
+				baseString = ValidSchemes.get(0) + "://" + ValidChars.get(0) + ValidChars.get(1) + ValidChars.get(2)
+						+ ValidChars.get(3) + ValidSchemes.get(4) + ValidChars.get(5) + ValidChars.get(6)
+						+ ValidChars.get(7) + ValidChars.get(8) + ValidChars.get(9);
+				expectedisValidResult = true;
+			} else if ((validityFlag == 1)) { // 1 = invalid scheme, other chars
+				errorMessage = "InValid Scheme and Valid URL text ";
+				baseString = InValidSchemes.get(0) + "://" + ValidChars.get(0) + ValidChars.get(1) + ValidChars.get(2)
+						+ ValidChars.get(3) + ValidSchemes.get(4) + ValidChars.get(5) + ValidChars.get(6)
+						+ ValidChars.get(7) + ValidChars.get(8) + ValidChars.get(9);
+				expectedisValidResult = false;
+
+			} else if ((validityFlag == 2)) { // 2 = scheme is valid, rest of URL is not, first char and fifth is allwys
+				errorMessage = "Valid Scheme and InValid URL text ";
+				// invalid,
+				baseString = ValidSchemes.get(0) + "://" + InValidChars.get(0) + ValidChars.get(1) + ValidChars.get(2)
+						+ ValidChars.get(3) + InValidChars.get(4) + ValidChars.get(5) + ValidChars.get(6)
+						+ ValidChars.get(7) + ValidChars.get(8) + ValidChars.get(9);
+				expectedisValidResult = false;
+
+			} else if ((validityFlag == 3)) { // 3 = scheme and rest is invalid
+				errorMessage = "InValid Scheme and InValid URL text ";
+				baseString = InValidSchemes.get(0) + "://" + InValidChars.get(0) + ValidChars.get(1) + ValidChars.get(2)
+						+ ValidChars.get(3) + InValidChars.get(4) + InValidChars.get(5) + ValidChars.get(6)
+						+ ValidChars.get(7) + ValidChars.get(8) + ValidChars.get(9);
+				expectedisValidResult = false;
+
+			}
+			result = (urlVal.isValid(baseString)); // "https://www.google.com")); //
+			// boolean result =
+			// (urlVal.isValid("ftp://username:password@ftp.fakesite.org/"));
+			// result = (urlVal.isValid("http://www.amazon.com"));
+			if (expectedisValidResult != result) {
+				System.out.println("Test Failed " + errorMessage + " using " + baseString);
+
+			}
+		}
+	}
+
+	////////////////////////////////////////
+	public void testIsValid1() {
+		Map<String, Boolean> schemes = new HashMap<String, Boolean>() {
+			{
+				put("http://", true);
+				put("http:/", false);
+				put("http:", false);
+				put("http", false);
+				put("", true);
+
+			}
+		};
+
+		Map<String, Boolean> authorities = generaterHashMap(1);
+		// new HashMap<String, Boolean>() {
+		// {
+		// put("www.google.com", true);
+		// put("test.com", true);
+		// put("test.tv", true);
+		// put("255.255.255.255", true);
+		// put("255.256.255.255", false);
+		//
+		// }
+		// };
+		// generaterHashMap(1);
+		Map<String, Boolean> paths = new HashMap<String, Boolean>() {
+			{
+				put("/test", true);
+				put("/test1", true);
+				put("/test/", true);
+				put("/test/otherthing/file", true);
+				put("//test", false);
+				put("/test//", false);
+
+			}
+		};
+
+		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+		// for(int i = 0; i < 10000; i++)
+
+		for (Map.Entry<String, Boolean> s : schemes.entrySet()) {
+			for (Map.Entry<String, Boolean> a : authorities.entrySet()) {
+				for (Map.Entry<String, Boolean> p : paths.entrySet()) {
+					// System.out.println(p.getKey());
+
+					if (Boolean.TRUE.equals(s.getValue()) && Boolean.TRUE.equals(a.getValue())
+							&& Boolean.TRUE.equals(p.getValue())) {
+						// assertTrue(urlVal.isValid(s.getKey() + a.getKey() + p.getKey()));
+						System.out.println(s.getKey() + a.getKey() + p.getKey());
+						System.out.println(
+								"urlVal.isValid() says " + urlVal.isValid(s.getKey() + a.getKey() + p.getKey()));
+						System.out.println("Predefined rules say true");
+						System.out.println("\n");
+					}
+
+					else {
+						// assertFalse(urlVal.isValid(s.getKey() + a.getKey() + p.getKey()));
+						System.out.println(s.getKey() + a.getKey() + p.getKey());
+						System.out.println(
+								"urlVal.isValid() says " + urlVal.isValid(s.getKey() + a.getKey() + p.getKey()));
+						System.out.println("Predefined rules say false");
+						System.out.println("\n");
+					}
+
+				}
+			}
+		}
+
+	}
+
+	///////////////////////////////////////////
 	public void testIsValid2() {
 		Boolean expectedResult, actualResult, allTestsPassed = true, printAllTestCases = false;
 
@@ -295,6 +548,41 @@ public class UrlValidatorTest extends TestCase {
 		int schemeArrayLoop = 0;
 		int allowAllSchemeLoop = 0;
 		int allow2SlashesFlag = 0;
+		// valid AND existing schemese, found here :
+		// https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml only last one
+		// is valid and non-existing
+
+		ArrayList DefaultSchemes = new ArrayList();
+		DefaultSchemes.add("http");
+		DefaultSchemes.add("https");
+		DefaultSchemes.add("ftp");
+
+		String[] validCharArray = { "" };
+		String[] InvalidCharArray;
+
+		ArrayList ValidSchemes = new ArrayList();
+		ValidSchemes.add("Go");
+		ValidSchemes.add("Gopher");
+		ValidSchemes.add("Http");
+		ValidSchemes.add("Https");
+		ValidSchemes.add("hxxp");
+		ValidSchemes.add("ms-secondary-screen-controller");
+		ValidSchemes.add("ni");
+		ValidSchemes.add("soap.beep");
+		ValidSchemes.add("telnet");
+		ValidSchemes.add("BooBooKitty");
+
+		ArrayList InValidSchemes = new ArrayList();
+		InValidSchemes.add("Go<");
+		InValidSchemes.add("Go pher");
+		InValidSchemes.add("Htt#");
+		InValidSchemes.add("H%tps");
+		InValidSchemes.add("hx#p");
+		InValidSchemes.add("ms<secondary-screen-controller");
+		InValidSchemes.add("=");
+		InValidSchemes.add("soap[.beep");
+		InValidSchemes.add("]elnet");
+		InValidSchemes.add("B'oBooKitty");
 
 		ArrayList ValidChars = new ArrayList();
 		ValidChars.add("A");
@@ -382,59 +670,35 @@ public class UrlValidatorTest extends TestCase {
 		int shuffleControllers = 0;
 		int loopController = 0;
 		String item = "";
-		int ValidCharsarrayLen = 0;
-		int ValidCharsarrayItem = 0;
-		int len = 10;
 		Random rand = new Random();
-		boolean invalidUrl = false;
-		boolean failTest;
-
-		paths.put("www.google.com", true);
-		paths.put("test.com", true);
-		paths.put("test.tv", true);
-		paths.put("255.255.255.255", true);
-		paths.put("255.256.255.255", false);
-
 		for (int i = 0; i < 100; i++) {
 			// validityFlag = i % 4;
-			item = "";
-			if (i % 10 == 0) {
-				len = 100; // one in 10 will be a random number between 1 and 100
-			} else {
-				len = 10;// 9 in 10 will be a random number between 1 and 100
-			}
-			urlLen = rand.nextInt(len) + 1; // 2082 is the longest valid url len, dont want alot of those, due to
-			if ((i == 30) || (i == 60)) {
-				urlLen = 2082;
-			}
-			failTest = ((i % 4) == 0);
-			// memory, does the urlvalidator test len limit?
-			shuffleControllers = i % 5;
-			if (shuffleControllers == 0) { // randomize every 5 loops
+			urlLen = rand.nextInt(2082) + 1; // 2082, does the urlvalidator test len limit?
+			shuffleControllers = i % 100;
+			if (shuffleControllers == 1) { // randomize every 100 loops
 				Collections.shuffle(ValidChars);
 				Collections.shuffle(InValidChars);
 			}
 			ValidChars.indexOf(ValidChars);
-			ValidCharsarrayLen = ValidChars.size();
-			ValidCharsarrayItem = 0;
-
+			int ValidCharsarrayLen = ValidChars.lastIndexOf(ValidChars);
+			int ValidCharsarrayItem = 0;
+			int InValidCharsarrayLen = ValidChars.lastIndexOf(ValidChars);
+			int InValidCharsarrayItem = 0;
+			item.concat("/");
 			for (loopController = 0; loopController < urlLen; loopController++) {
 				ValidCharsarrayItem++;
-				if (ValidCharsarrayItem >= ValidCharsarrayLen) {
+				if (ValidCharsarrayItem > ValidCharsarrayLen) {
 					ValidCharsarrayItem = 0;
 				}
-				if ((failTest) && (ValidCharsarrayItem == (urlLen - 1))) {
-					item += (String) InValidChars.get(5);
-				} else {
-					item += (String) ValidChars.get(ValidCharsarrayItem);
-				}
+				item += (String) ValidChars.get(ValidCharsarrayItem);
 
 			}
-			paths.put(item, !failTest);
+			paths.put(item, false);
 		}
 		return paths;
 	}
 
+	////////////////////////////////////////
 	public void testAnyOtherUnitTest() {
 
 	}
